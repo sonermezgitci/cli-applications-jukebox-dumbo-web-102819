eb9d414e99c0b4ae7e0d92769003b3109cb6cea6
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-29 03:04:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,3 +1,7 @@
 def help
-puts "/n " 
+puts "/n I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program" 
 end 
