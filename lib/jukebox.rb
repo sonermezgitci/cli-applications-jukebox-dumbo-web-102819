7db0084781c1f896db756641eadda2aa9912db5b
Soner Mezgitci <mezgitci9@gmail.com>
2019-10-29 03:12:25 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,5 +1,5 @@
 def help
- puts "I accept the following commands:
+ puts "/nI accept the following commands:
 - help : displays this help message
 - list : displays a list of songs you can play
 - play : lets you choose a song to play
